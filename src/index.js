7e375c3837c2a1d8e298df13e64aec17cc95f427
Staffan Sandberg <staffansandberg@outlook.com>
2021-02-12 18:54:41 +0100

    simple scene with one cube

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import * as THREE from 'three';
 
 function main() {
+  // Create a renderer and append it to our document
   const renderer = new THREE.WebGLRenderer();
+  document.body.append(renderer.domElement);
 
-  console.log(renderer.domElement);
-
+  // Create a perspective camera and move it back 2 units
   const fov = 75;
   const aspect = 2;
   const near = 0.1;
   const far = 5;
   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-
   camera.position.z = 2;
+
+  const scene = new THREE.Scene();
+
+  // Create a cube and add it to the scene
+  const boxWidth = 1;
+  const boxHeight = 1;
+  const boxDepth = 1;
+  const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+  const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 });
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  renderer.render(scene, camera);
 }
 main();
 console.log('HELLO');
